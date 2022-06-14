9c019e8421ec1019e223787ce7cf780656876696
abbat01 <95858072+abbat01@users.noreply.github.com>
2022-06-14 23:36:31 +0300

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const telegram = document.querySelector('.fa-telegram-plane');
-
-telegram.addEventListener('mouseout', function() {
-    const div = document.createElement('div')
-    div.style.backgroundColor = 'red'
-    div.textContent = 'Hi!'
-    telegram.append(div)
-}, { once: true })
